dc9e55098913b13bd2fe705d95b676f7deb27827
dzucwf <dz_uc@163.com>
2015-01-19 14:10:33 +0800

    初始加载

---
+++ app/src/androidTest/java/com/safeness/e_safeness/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.safeness.e_safeness;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
