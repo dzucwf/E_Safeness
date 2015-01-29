ba117c9c4a888081453de479f2de70aa2f62d876
lionnd <lionnd@gmail.com>
2015-01-30 04:51:01 +0800

    2015.1.22 0:19

--- patient_saveness/src/main/java/com/safeness/patient/ui/activity/DrugSettingActivity.java
+++ patient_saveness/src/main/java/com/safeness/patient/ui/activity/DrugSettingActivity.java
@@ -1,25 +1,144 @@
 package com.safeness.patient.ui.activity;
 
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 import com.safeness.e_saveness_common.base.AppBaseActivity;
+import com.safeness.e_saveness_common.dao.DaoFactory;
+import com.safeness.e_saveness_common.dao.IBaseDao;
 import com.safeness.patient.R;
 import com.safeness.patient.adapter.BtmNaviSwitchAdapter;
+import com.safeness.patient.model.Drug;
+import com.safeness.patient.model.U_d;
+
+import java.sql.Time;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 /**
  * Created by Lionnd on 2015/1/29.
  */
 public class DrugSettingActivity extends AppBaseActivity {
+
+    private ArrayList<Map<String,Object>> mData= new ArrayList<Map<String,Object>>();
+    private  IBaseDao<Drug> drugDao;
+    private  IBaseDao<U_d> u_dDao;
+    List<Drug> drugList;
+
+    private TextView txb_desc;
+    private Button btn_finish;
+    private TextView txb_name;
+    private ListView lst_remind_list;
+    private TextView txb_remind;
+
+    private ImageButton btn_back;
+    private ImageView img_cell_remind;
+    private ImageView img_cell_addRemind;
+
+    private ImageView img_cell_switch;
+
+    private MyAdapter adapter;
+
+    private Integer _id = -1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        /*取出Intent中附加的数据*/
+        Intent intent =getIntent();
+        _id = intent.getIntExtra("drug_id", -1);
+
+        if (_id > 0){
+            drugDao = DaoFactory.createGenericDao(this, Drug.class);
+            drugList = drugDao.queryByCondition("_id=?",_id.toString());
+
+            if (drugList.size() >0){
+                txb_name.setText(drugList.get(0).getDrugName());
+                txb_desc.setText(drugList.get(0).getDesc());
+                txb_remind.setText(drugList.get(0).getLife_status() > 0 ? "提醒(已开启)" : "提醒(已关闭)");
+                img_cell_switch.setBackgroundResource(drugList.get(0).getLife_status() > 0 ?R.drawable.switch_on : R.drawable.switch_off);
+
+                u_dDao = DaoFactory.createGenericDao(this, U_d.class);
+                List<U_d> u_dList = u_dDao.queryByCondition("u_sid=? and d_id=?","1",_id.toString());
+                mData= new ArrayList<Map<String,Object>>();
+                for(int i =0; i < u_dList.size(); i++) {
+                    Map<String,Object> item = new HashMap<String,Object>();
+                    item.put("title", u_dList.get(i).getHintTime());
+                    item.put("status", u_dList.get(i).getLife_status());
+                    item.put("id", u_dList.get(i).get_id());
+                    mData.add(item);
+                }
+                adapter = new MyAdapter(this,mData,R.layout.drug_setting_listitem,
+                        new String[]{"title","status"},new int[]{R.id.drug_setting_remind_list_label_time,R.id.drug_setting_remind_list_image_status});
+                lst_remind_list.setAdapter(adapter);
+
+                btn_back.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        DrugSettingActivity.this.finish();
+                    }
+                });
+
+                img_cell_remind.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        drugList.get(0).setLife_status(drugList.get(0).getLife_status() > 0 ? -1 : 1);
+                        if(drugDao.update(drugList.get(0),"_id=?",_id.toString())){
+                            drugList = drugDao.queryByCondition("_id=?",_id.toString());
+                            txb_remind.setText(drugList.get(0).getLife_status() > 0 ? "提醒(已开启)" : "提醒(已关闭)");
+                            img_cell_switch.setBackgroundResource(drugList.get(0).getLife_status() > 0 ?R.drawable.switch_on : R.drawable.switch_off);
+                            switchVisible();
+                        }
+
+                    }
+                });
+                img_cell_addRemind.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        showDialog(R.id.drug_setting_remind_list_label_time);
+                    }
+                });
+                switchVisible();
+            }
+
+
+        }
 
     }
+    private void switchVisible(){
+        img_cell_addRemind.setVisibility(drugList.get(0).getLife_status() > 0 ? View.VISIBLE: View.INVISIBLE);
+        TextView textView = (TextView)findViewById(R.id.textView12);
+        textView.setVisibility(drugList.get(0).getLife_status() > 0 ? View.VISIBLE: View.INVISIBLE);
+        ImageView imageView = (ImageView)findViewById(R.id.imageView12);
 
+        imageView.setVisibility(drugList.get(0).getLife_status() > 0 ? View.VISIBLE: View.INVISIBLE);
+        lst_remind_list.setVisibility(drugList.get(0).getLife_status() > 0 ? View.VISIBLE: View.INVISIBLE);
+        btn_finish.setVisibility(drugList.get(0).getLife_status() > 0 ? View.VISIBLE: View.INVISIBLE);
+
+    }
     @Override
     protected int getLayoutId() {
         return R.layout.drug_setting;
@@ -37,7 +156,116 @@ public class DrugSettingActivity extends AppBaseActivity {
 
     //初始化下层切换
     private void getViews() {
+        txb_desc = (TextView)findViewById(R.id.txb_drug_setting_desc);
+        txb_desc.setMovementMethod(ScrollingMovementMethod.getInstance());
+        btn_finish = (Button)findViewById(R.id.btn_drug_setting_finish);
+        txb_name = (TextView)findViewById(R.id.txb_drug_setting_name);
+        txb_remind = (TextView)findViewById(R.id.txv_drugsetting_cell_remind);
+        lst_remind_list = (ListView)findViewById(R.id.drug_setting_remind_list);
 
+        btn_back = (ImageButton)findViewById(R.id.btn_drugsetting_back);
+        img_cell_remind = (ImageView)findViewById(R.id.img_drugsetting_cell_bg3);
+        img_cell_addRemind = (ImageView)findViewById(R.id.img_drugsetting_cell_bg4);
+        img_cell_switch = (ImageView)findViewById(R.id.img_drugsetting_cell_remind_switch);
+
+        lst_remind_list = (ListView)findViewById(R.id.drug_setting_remind_list);
+        ArrayList<Map<String,Object>> mData= new ArrayList<Map<String,Object>>();
+
+
+
+
+    }
+    @Override
+    protected Dialog onCreateDialog(int id)
+    {
+        switch (id) {
+            case R.id.drug_setting_remind_list_label_time:
+                Date curDate   =   new   Date(System.currentTimeMillis());
+                return  new TimePickerDialog(
+                        this, mTimeSetListener, curDate.getHours(), curDate.getMinutes(), true);
+        }
+        return null;
+    }
+    private TimePickerDialog.OnTimeSetListener mTimeSetListener =
+            new TimePickerDialog.OnTimeSetListener()
+            {
+                public void onTimeSet(
+                        TimePicker view, int hourOfDay, int minuteOfHour)
+                {
+
+                    SimpleDateFormat timeFormat = new SimpleDateFormat("1970-1-1 hh:mm:00");
+                    Date date = new Date(0,0,0, hourOfDay, minuteOfHour);
+                    String strDate = timeFormat.format(date);
+                    Log.v("v",strDate);
+                    U_d u_d = new U_d();
+                    u_d.setU_sid(1);
+                    u_d.setD_id(_id);
+                    u_d.setCount(-1);
+                    u_d.setHintDay("2000-1-1");
+                    u_d.setHintTime(strDate);
+                    if(u_dDao.insert(u_d)){
+                        HashMap<String ,Object> map = new HashMap<String, Object>();
+                        SimpleDateFormat ttt = new SimpleDateFormat("hh:mm");
+                        SimpleDateFormat dt = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+                        try{
+                            date = ConvertToDate(strDate);
+
+                            map.put("title",ttt.format(date));}
+                        catch(Exception ex){}
+                        map.put("status", 1);
+                        mData.add(map);
+                        adapter.mItemList = mData;
+                        adapter.notifyDataSetChanged();
+                    }
+                }
+            };
+    private class MyAdapter extends SimpleAdapter{
+        int count = 0;
+        private List<Map<String, Object>> mItemList;
+        public MyAdapter(Context context, List<? extends Map<String, Object>> data,
+                         int resource, String[] from, int[] to) {
+            super(context, data, resource, from, to);
+            mItemList = (List<Map<String, Object>>) data;
+            if(data == null){
+                count = 0;
+            }else{
+                count = data.size();
+            }
+        }
+        public int getCount() {
+            return mItemList.size();
+        }
+
+        public Object getItem(int pos) {
+            return pos;
+        }
+
+        public long getItemId(int pos) {
+            return pos;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            Map<String ,Object> map = mItemList.get(position);
+            View view = super.getView(position, convertView, parent);
+            ImageView imageview = (ImageView)view.findViewById(R.id.drug_setting_remind_list_image_status);
+            TextView tv = (TextView)view.findViewById(R.id.drug_setting_remind_list_label_time);
+            imageview.setVisibility(Integer.parseInt(map.get("status").toString())>0 ? View.VISIBLE : View.INVISIBLE);
+            SimpleDateFormat ttt = new SimpleDateFormat("hh:mm");
+            SimpleDateFormat dt = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+            try{
+            Date date = ConvertToDate(map.get("title").toString());
+
+            tv.setText(ttt.format(date));}
+            catch(Exception ex){}
+            return view;
+        }
+
+
+    }
+    private  Date ConvertToDate(String strDate) throws Exception{
+        SimpleDateFormat dt = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        return dt.parse(strDate);
 
     }
 }
--- patient_saveness/src/main/java/com/safeness/patient/ui/fragment/NaviDrugFragment.java
+++ patient_saveness/src/main/java/com/safeness/patient/ui/fragment/NaviDrugFragment.java
@@ -2,22 +2,33 @@ package com.safeness.patient.ui.fragment;
 
 import android.app.Fragment;
 import android.app.FragmentTransaction;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
 import com.safeness.e_saveness_common.base.AppBaseFragment;
+import com.safeness.e_saveness_common.dao.DaoFactory;
+import com.safeness.e_saveness_common.dao.IBaseDao;
 import com.safeness.patient.R;
+import com.safeness.patient.model.Drug;
+import com.safeness.patient.ui.activity.DrugSettingActivity;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.TextView;
 
 //用药
 public class NaviDrugFragment extends AppBaseFragment {
@@ -26,7 +37,7 @@ public class NaviDrugFragment extends AppBaseFragment {
     private ListView listView;
 
     private ArrayList<Map<String,Object>> mData= new ArrayList<Map<String,Object>>();
-    private SimpleAdapter adapter = null;
+    private MyAdapter adapter = null;
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
 
@@ -56,15 +67,73 @@ public class NaviDrugFragment extends AppBaseFragment {
 
        mData= new ArrayList<Map<String,Object>>();
 
-        for(int i =0; i < 20; i++) {
+        IBaseDao<Drug> drugDao = DaoFactory.createGenericDao(getActivity(), Drug.class);
+
+
+
+
+
+        List<Drug> drugList = drugDao.queryByCondition("");
+
+        Drug drug = new Drug();
+        if (drugList.isEmpty()){//写入测试数据
+
+            drug.setDrugName("格列齐特缓释片");
+            drug.setDesc("当单用饮食疗法、运动治疗和减轻体重不足以控制血糖水平的成人非胰岛素依赖型糖尿病（2型）。");
+            drugDao.insert(drug);
+
+            drug.setDrugName("拜唐苹(阿卡波糖片)");
+            drug.setDesc("配合饮食控制，用于：1.II型糖尿病。2.降低糖耐量减低者的餐后血糖。");
+            drugDao.insert(drug);
+
+            drug.setDrugName("诺和龙(瑞格列奈片)");
+            drug.setDesc("用于饮食控制、减轻体重及运动锻炼不能有效控制其高血糖的2型糖尿病（非胰岛素依赖型）患者。瑞格列奈片可与二甲双胍并用。二者并用时对控制血糖比各自单独");
+            drugDao.insert(drug);
+
+            drug.setDrugName("格华止(盐酸二甲双胍片)");
+            drug.setDesc("1.本品首选用于单纯饮食控制及体育锻炼治疗无效的2型糖尿病，特别是肥胖的2型糖尿病。2.对于1型或2型糖尿病，本品与胰岛素合用，可增加胰岛素的降血糖作用");
+            drugDao.insert(drug);
+
+            drug.setDrugName("消渴丸");
+            drug.setDesc("滋肾养阴，益气生津。用于多饮，多尿，多食，消瘦，体倦无力，眠差腰痛，尿糖及血糖升高之气阴两虚型消渴症。");
+            drugDao.insert(drug);
+
+            drug.setDrugName("糖脉康颗粒");
+            drug.setDesc("养阴清热，活血化瘀，益气固肾。用于气阴两虚血瘀所致的口渴喜饮，倦怠乏力，气短懒言，自汗，盗汗，五心烦热，胸中闷痛，肢体麻木或刺痛，便秘，糖尿病Ⅱ");
+            drugDao.insert(drug);
+
+            drug.setDrugName("降糖宁胶囊");
+            drug.setDesc("益气，养阴，生津。用于糖尿病属气阴两虚者。");
+            drugDao.insert(drug);
+
+            drug.setDrugName("金路捷(注射用鼠神经生长因子)");
+            drug.setDesc("正已烷中毒性周围神经病。");
+            drugDao.insert(drug);
+
+            drug.setDrugName("盐酸吡格列酮片");
+            drug.setDesc("对于2型糖尿病（非胰岛素依赖性糖尿病，NIDDM）患者，盐酸吡格列酮可与饮食控制和体育锻炼联合以改善血糖控制。盐酸吡格列酮可单独使用，当饮食控制、体育");
+            drugDao.insert(drug);
+
+            drug.setDrugName("玉泉丸");
+            drug.setDesc("养阴生津，止渴除烦，益气和中。用于治疗因胰岛功能减退而引起的物质代谢、碳水化合物代谢紊乱，血糖升高之糖尿病(亦称消渴症)，肺胃肾阴亏损，热病后期。");
+            drugDao.insert(drug);
+
+            drug.setDrugName("糖适平(格列喹酮片)");
+            drug.setDesc("2型糖尿病（即非胰岛素依赖型糖尿病）。");
+            drugDao.insert(drug);
+
+            drugList = drugDao.queryByCondition("life_status>0");
+        }
+
+        for(int i =0; i < drugList.size(); i++) {
             Map<String,Object> item = new HashMap<String,Object>();
-            item.put("title", "弗莱明");
-            item.put("imgAlert", i%2>0);
-            item.put("_id", i+1);
+            item.put("title", drugList.get(i).getDrugName());
+            item.put("imgAlert", drugList.get(i).getLife_status());
+            item.put("_id",drugList.get(i).get_id());
             mData.add(item);
         }
-        adapter = new SimpleAdapter(getActivity(),mData,R.layout.drug_listitem,
-                new String[]{"title","imgAlert"},new int[]{R.id.drug_listview_item_title,R.id.drug_listview_item_imgAlert});
+        adapter = new MyAdapter(getActivity(),mData,R.layout.drug_listitem,
+                new String[]{"title","imgAlert","_id"},new int[]{R.id.drug_listview_item_title,R.id.drug_listview_item_imgAlert});
         listView.setAdapter(adapter);
         listView.setOnItemClickListener(new OnItemClickListener() {
             @Override
@@ -75,20 +144,61 @@ public class NaviDrugFragment extends AppBaseFragment {
                 Object title = map.get("title");
                 //NaviDrugFragment.this.info.setText("选中的数据项ID是：" + _id + "，名称是："+ name);
                 Log.v("e", "选中的数据项ID是：" + _id + "，名称是：" + title);
-
+/*
                 android.support.v4.app.Fragment drug_settingFr = new Drug_SettingFragment();
 
                 android.support.v4.app.FragmentTransaction ft = getActivity().getSupportFragmentManager().beginTransaction();
                 ft.add(R.id.navi_view_pager, drug_settingFr);
                 ft.addToBackStack(null);
-                ft.commit();
+                ft.commit();*/
+                Intent in=new Intent(getActivity(),DrugSettingActivity.class);
+                in.putExtra("drug_id",  Integer.parseInt(_id.toString()));
+                startActivity(in);
             }
 
         });
 
     }
 
+    private class MyAdapter extends SimpleAdapter{
+        int count = 0;
+        private List<Map<String, Object>> mItemList;
+        public MyAdapter(Context context, List<? extends Map<String, Object>> data,
+                         int resource, String[] from, int[] to) {
+            super(context, data, resource, from, to);
+            mItemList = (List<Map<String, Object>>) data;
+            if(data == null){
+                count = 0;
+            }else{
+                count = data.size();
+            }
+        }
+        public int getCount() {
+            return mItemList.size();
+        }
 
+        public Object getItem(int pos) {
+            return pos;
+        }
+
+        public long getItemId(int pos) {
+            return pos;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            Map<String ,Object> map = mItemList.get(position);
+            View view = super.getView(position, convertView, parent);
+            ImageView imageview = (ImageView)view.findViewById(R.id.drug_listview_item_imgAlert);
+            TextView tv = (TextView)view.findViewById(R.id.drug_listview_item_title);
+            imageview.setVisibility(Integer.parseInt(map.get("imgAlert").toString())>0 ? View.VISIBLE : View.INVISIBLE);
+
+            tv.setText(map.get("title").toString());
+            return view;
+        }
+
+
+    }
     private List<String> getData(){
 
         List<String> data = new ArrayList<String>();
