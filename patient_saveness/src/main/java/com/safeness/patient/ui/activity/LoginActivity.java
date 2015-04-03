cdecf3873d350882d4b4976ca8504bd973783593
lionnd <lionnd@gmail.com>
2015-04-03 17:23:02 +0800

    2015.1.22 0:19

--- patient_saveness/src/main/java/com/safeness/patient/ui/activity/LoginActivity.java
+++ patient_saveness/src/main/java/com/safeness/patient/ui/activity/LoginActivity.java
@@ -78,6 +78,19 @@ public class LoginActivity extends AppBaseActivity implements LoaderManager.Load
 
     Context mContext;
 
+    /**
+     * 监听Back键按下事件
+     * 注意:
+     * super.onBackPressed()会自动调用finish()方法,关闭
+     * 当前Activity.
+     * 若要屏蔽Back键盘,注释该行代码即可
+     */
+    @Override
+    public void onBackPressed() {
+        //super.onBackPressed();
+        System.out.println("按下了back键 onBackPressed()");
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
